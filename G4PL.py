9143cf91bc59498c5b3bbe74e98cbe0af7f938f7
Giuseppe Cosentino <giuseppe.cosentino@cnr.it>
2026-02-19 15:41:55 +0100

    Add files via upload

---
+++ G4PL.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Giuseppe Cosentino'
+__date__ = '2026-02-19'
+__copyright__ = '(C) 2025 by Giuseppe Cosentino'
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
+import os
+import sys
+import inspect
+
+from qgis.core import QgsProcessingAlgorithm, QgsApplication
+from .G4PL_provider import GeologyProvider
+
+cmd_folder = os.path.split(inspect.getfile(inspect.currentframe()))[0]
+
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+
+class GeologyPlugin(object):
+
+    def __init__(self):
+        self.provider = None
+
+    def initProcessing(self):
+        """Init Processing provider for QGIS >= 3.8."""
+        self.provider = GeologyProvider()
+        QgsApplication.processingRegistry().addProvider(self.provider)
+
+    def initGui(self):
+        self.initProcessing()
+
+    def unload(self):
+        QgsApplication.processingRegistry().removeProvider(self.provider)
